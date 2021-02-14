f8a2b26ddd41a0b6b20545144616df671e7234e6
gassdev <gassdev@gmail.com>
2021-02-14 21:22:04 +0000

    Refactoring of create/edit by using PinType and make use of PUT as method for edit

--- src/Controller/PinsController.php
+++ src/Controller/PinsController.php
@@ -3,14 +3,13 @@
 namespace App\Controller;
 
 use App\Entity\Pin;
+use App\Form\PinType;
 use App\Repository\PinRepository;
 use Doctrine\ORM\EntityManagerInterface;
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\Form\Extension\Core\Type\TextareaType;
-use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class PinsController extends AbstractController
 {
@@ -34,12 +33,7 @@ class PinsController extends AbstractController
         $pin = new Pin;
 
 
-        $form = $this
-            ->createFormBuilder($pin)
-            ->add('title', TextType::class)
-            ->add('description', TextareaType::class)
-            ->getForm()
-        ;
+        $form = $this->createForm(PinType::class, $pin);
 
         $form->handleRequest($request);
         
@@ -67,16 +61,13 @@ class PinsController extends AbstractController
 
 
     /**
-     * @Route("/pin/{id<[0-9]+>}/edit/", name="app_pins_edit", methods={"GET", "POST"})
+     * @Route("/pin/{id<[0-9]+>}/edit/", name="app_pins_edit", methods={"GET", "PUT"})
      */
     public function edit(Request $request, Pin $pin, EntityManagerInterface $em): Response
     {
-        $form = $this
-            ->createFormBuilder($pin)
-            ->add('title', TextType::class)
-            ->add('description', TextareaType::class)
-            ->getForm()
-        ;
+        $form = $this->createForm(PinType::class, $pin, [
+            'method' => 'PUT'
+        ]);
 
         $form->handleRequest($request);
 
