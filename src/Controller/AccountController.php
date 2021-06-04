13ecad210d3731823937f711004bc16e9683950c
gassdev <gassdev@gmail.com>
2021-06-04 12:38:24 +0000

    add change password feature

--- src/Controller/AccountController.php
+++ src/Controller/AccountController.php
@@ -3,18 +3,20 @@
 namespace App\Controller;
 
 use App\Form\UserFormType;
+use App\Form\ChangePasswordFormType;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
 
 class AccountController extends AbstractController
 {
     /**
      * @Route("/account", name="app_account", methods="GET")
      */
-    public function index(): Response
+    public function show(): Response
     {
         return $this->render('account/show.html.twig');
     }
@@ -39,6 +41,39 @@ class AccountController extends AbstractController
         }
 
         return $this->render('account/edit.html.twig', [
+            'form' => $form->createView(),
+        ]);
+    }
+
+    /**
+     * @Route("/account/change-password", name="app_account_change_password", methods={"GET", "POST"})
+     */
+    public function changePassword(
+        Request $request,
+        EntityManagerInterface $em,
+        UserPasswordEncoderInterface $passwordEncoder
+    ): Response {
+        $user = $this->getUser();
+        $form = $this->createForm(ChangePasswordFormType::class);
+
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $user->setPassword(
+                $passwordEncoder->encodePassword(
+                    $user,
+                    $form->get('newPassword')->getData()
+                )
+            );
+
+            $em->flush();
+
+            $this->addFlash('success', 'Password updated successfully!');
+
+            return $this->redirectToRoute('app_account');
+        }
+
+        return $this->render('account/change_password.html.twig', [
             'form' => $form->createView(),
         ]);
     }
