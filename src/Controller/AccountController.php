7f3807b2c696bd17963828a5a6fe317479771b84
gassdev <gassdev@gmail.com>
2021-06-12 21:52:31 +0000

    add control access for account routes

--- src/Controller/AccountController.php
+++ src/Controller/AccountController.php
@@ -8,11 +8,13 @@ use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\IsGranted;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
 
 /**
  * @Route("/account")
+ * @IsGranted("ROLE_USER")
  */
 class AccountController extends AbstractController
 {
